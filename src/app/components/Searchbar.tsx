b478ba49073e7b47aa0ba49ebeef26e4ac639f66
Maykel <jotasenator@gmail.com>
2023-10-09 19:18:32 -0400

    Implement the scrape functionality and the next.config.js

--- src/app/components/Searchbar.tsx
+++ src/app/components/Searchbar.tsx
@@ -1,18 +1,60 @@
 "use client";
 
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import { scrapeAndStoreProduct } from "../lib/actions";
 
 const Searchbar = () => {
-  const handleSubmit = () => {};
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const isValidAmazonURL = (url: string) => {
+    try {
+      const parsedURL = new URL(url);
+      const hostname = parsedURL.hostname;
+      if (
+        hostname.includes("amazon.com") ||
+        hostname.includes("amazon.") ||
+        hostname.endsWith("amazon")
+      ) {
+        return true;
+      }
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const isValidLink = isValidAmazonURL(searchPrompt);
+
+    !isValidLink ? alert("Please provide a valid Amazon link") : "";
+
+    try {
+      setIsLoading(true);
+      //Scrape the product
+      const product = await scrapeAndStoreProduct(searchPrompt);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <form className="flex flex-wrap gpap-4 mt-12" onSubmit={handleSubmit}>
       <input
         type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
         placeholder="Enter product link"
         className="searchbar-input"
       />
-      <button type="submit" className="searchbar-btn">
-        Search
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === ""}
+      >
+        {isLoading ? "Searching..." : "Search"}
       </button>
     </form>
   );
